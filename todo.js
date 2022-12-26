1c742ae371b5e55edd478ff6070e79b4309214c7
harksu <harksulim@naver.com>
2022-12-27 01:42:51 +0900

    feat : update(local storage)

--- todo.js
+++ todo.js
@@ -6,7 +6,7 @@ const key = "todoProject";
 
 let toDoEvents = [];
 
-const saveTodo = () => {
+const saveToDo = () => {
   localStorage.setItem(key, JSON.stringify(toDoEvents)); //일단 저장
 };
 
@@ -21,22 +21,50 @@ function deleteTodo(event) {
 
   toDoEvents = newToDoEvents;
 
-  saveTodo();
+  saveToDo();
+}
+
+function updateToDo(event) {
+  const btn = event.target;
+  const span = btn.previousSibling;
+  const li = btn.parentNode;
+  const newTodoContent = window.prompt("type update todo content");
+  span.text = newTodoContent;
+
+  const newToDoEvents = toDoEvents?.forEach((todo) => {
+    if (todo.id === parseInt(li.id)) {
+      todo.text = newTodoContent;
+      console.log(todo);
+    }
+  });
+
+  //toDoEvents = newToDoEvents;
+
+  saveToDo();
+  init();
+  //console.log(newTodoContent);
 }
 
 const showToDo = (text) => {
   const li = document.createElement("li"); //리스트에
   const deleteBtn = document.createElement("button"); //버튼만들고
+  const updateBtn = document.createElement("button");
   const span = document.createElement("span"); //내용 보여주고
   const newid = toDoEvents.length + 1; //식별자
 
   deleteBtn.innerText = "❌";
   deleteBtn.addEventListener("click", deleteTodo);
+
   //this 바인딩 문제로 여기 콜백함수로는 화살표 함수를 사용하면 안된다고 한다.
+
+  updateBtn.innerText = "🖍️";
+  updateBtn.addEventListener("click", updateToDo);
+  //deleteBtn.addEventListener("click", deleteTodo);
 
   span.innerHTML = text;
 
   li.appendChild(span); // 자식 노드 추가
+  li.appendChild(updateBtn);
   li.appendChild(deleteBtn); // 자식 노드 추가
   toDoList.appendChild(li); // 자식 노드 추가(내용물 안에)
   li.id = newid; //삭제를 위한 식별자 추가
@@ -46,7 +74,7 @@ const showToDo = (text) => {
     id: newid,
   };
   toDoEvents.push(toDoContent);
-  saveTodo();
+  saveToDo();
 };
 
 const handleSubmit = (event) => {
@@ -57,7 +85,7 @@ const handleSubmit = (event) => {
 
 const getToDoEvents = () => {
   const StoredToDoEvents = localStorage.getItem(key);
-  if (!StoredToDoEvents) return;
+  if (StoredToDoEvents) return;
   const storedToDoContent = JSON.parse(StoredToDoEvents);
   storedToDoContent?.forEach((todo) => {
     showToDo(todo.text);
